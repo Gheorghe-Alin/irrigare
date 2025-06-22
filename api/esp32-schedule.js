77642768b5a85afdad598ff9e62df8d59694e12d
Alin Pante <alin@example.com>
2025-06-22 21:54:34 +0300

    da

--- api/esp32-schedule.js
+++ api/esp32-schedule.js
@@ -5,10 +5,13 @@ const uri = process.env.MONGODB_URI;
 let cachedClient = null;
 
 export default async function handler(req, res) {
-  const { id, checkOnly } = req.query;
+  const { id } = req.query;
 
   if (!id) {
+
     return res.status(400).json({ error: "Missing device ID in query" });
+
+
   }
 
   try {
@@ -21,23 +24,23 @@ export default async function handler(req, res) {
     const collection = db.collection('schedules');
 
     const now = moment().tz("Europe/Bucharest");
+
     const currentDay = now.format("dddd").toLowerCase();
+
+
     const hour = now.hour();
     const minute = now.minute();
 
-    const filter = {
+    const active = await collection.findOne({
       deviceId: id.toLowerCase(),
       day: currentDay,
       hour,
+      minute,
+
       active: true,
-    };
 
-    // dacă este doar o verificare în timpul execuției, ignorăm minutul
-    if (checkOnly !== 'true') {
-      filter.minute = minute;
-    }
+    });
 
-    const active = await collection.findOne(filter);
     res.status(200).json(active || {});
   } catch (err) {
     res.status(500).json({ error: 'Eroare MongoDB', details: err.message });
