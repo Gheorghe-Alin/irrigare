1db9e5d91e50e3c3460cf8f147947f25db405c6f
Gheorghe-Alin <gheorghealin0@yahoo.com>
2025-08-07 12:05:56 +0300

    inapoi

--- api/esp32-schedule.js
+++ api/esp32-schedule.js
@@ -1,4 +1,3 @@
-Schedule;
 import moment from "moment-timezone";
 import { MongoClient } from "mongodb";
 
@@ -6,8 +5,11 @@ const uri = process.env.MONGODB_URI;
 let cachedClient = null;
 
 export default async function handler(req, res) {
-  const { id, checkOnly } = req.query;
-  if (!id) return res.status(400).json({ error: "Missing device ID" });
+  const { id } = req.query;
+
+  if (!id) {
+    return res.status(400).json({ error: "Missing device ID in query" });
+  }
 
   try {
     if (!cachedClient) {
@@ -19,24 +21,23 @@ export default async function handler(req, res) {
     const collection = db.collection("schedules");
 
     const now = moment().tz("Europe/Bucharest");
-    const day = now.format("dddd").toLowerCase();
+
+    const currentDay = now.format("dddd").toLowerCase();
+
     const hour = now.hour();
     const minute = now.minute();
 
-    const filter = {
+    const active = await collection.findOne({
       deviceId: id.toLowerCase(),
-      day,
+      day: currentDay,
       hour,
+      minute,
+
       active: true,
-    };
+    });
 
-    if (checkOnly !== "true") {
-      filter.minute = { $gte: minute }; // ⬅️ doar în sus!
-    }
-
-    const match = await collection.findOne(filter);
-    res.status(200).json(match || {});
+    res.status(200).json(active || {});
   } catch (err) {
-    res.status(500).json({ error: "MongoDB Error", details: err.message });
+    res.status(500).json({ error: "Eroare MongoDB", details: err.message });
   }
 }
